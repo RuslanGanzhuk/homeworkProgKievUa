728871d300445f10121b5fb4a936d2698be328f3
Ruslan <hanzhukruslan@gmail.com>
2017-05-27 20:26:48 +0300

    create comment for vector

---
+++ src/com/progKievUa/lesson1/task4/Runner.java
@@ -0,0 +1,13 @@
+package com.progKievUa.lesson1.task4;
+
+public class Runner {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
